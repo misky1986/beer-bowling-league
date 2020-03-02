cbaa03ff11ba854a4179e37f452418d29cdc66e3
Michael Skydt <mskydt86@gmail.com>
2020-03-02 07:52:49 +0100

    Added posibility of setting up integration test with in memory database,

---
+++ beer-bowling-league-api.tests/Helpers/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using beer_bowling_league_api.DataAccess;
+using beer_bowling_league_api.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace beer_bowling_league_api.tests.Helpers
+{
+    public class Utilities
+    {
+        public static void InitializeDbForTests(ApplicationDbContext db)
+        {
+            db.Players.AddRange(GetSeedingMessages());
+            db.SaveChanges();
+        }
+
+        public static void ReinitializeDbForTests(ApplicationDbContext db)
+        {
+            db.Players.RemoveRange(db.Players);
+            InitializeDbForTests(db);
+        }
+        
+        public static List<Player> GetSeedingMessages()
+        {
+            return new List<Player>()
+            {
+                new Player() { Age = 50, BirthDay =  new DateTime(1970, 9, 25), Alias = "Sucker", FirstName = "Dave", LastName = "Johnson" },
+                new Player() { Age = 33, BirthDay =  new DateTime(1987, 2, 6), Alias = "Mambo", FirstName = "Jordan", LastName = "Cramp" },
+                new Player() { Age = 100, BirthDay =  new DateTime(1920, 2, 6), Alias = "OldyJohn", FirstName = "John", LastName = "Doe" },
+            };
+        }
+    }
+}
---
+++ beer-bowling-league-api.tests/IntegrationTest/Controller/PlayerControllerIntegrationTests.cs
@@ -0,0 +1,55 @@
+﻿using beer_bowling_league_api.Contracts;
+using beer_bowling_league_api.tests.Helpers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace beer_bowling_league_api.tests.IntegrationTest.Controller
+{
+    public class PlayerControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<beer_bowling_league_api.Startup>>
+    {
+
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+        public PlayerControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task GetAll_WithoutAnyPosts_ReturnsEmptyResponse()
+        {
+            // Arrange
+                  
+            // Act
+            var response = await _client.GetAsync(ApiRoutes.Players.GetAll);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);            
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPost_WhenPostExistInTheDataBase()
+        {
+            // Arrange
+            //await AuthenticateAsync();
+            //var createdPost = await CreatePostAsync(new CreatePostRequest { Name = "Test post" });
+
+            //// Act
+            //var response = await TestClient.GetAsync(ApiRoutes.Posts.Get.Replace("{postId}", createdPost.Id.ToString()));
+
+            //// Assert
+            //response.StatusCode.Should().Be(HttpStatusCode.OK);
+            //var returnedPost = await response.Content.ReadAsAsync<Post>();
+            //returnedPost.Id.Should().Be(createdPost.Id);
+            //returnedPost.Name.Should().Be("Test post");
+        }
+    }
+}
